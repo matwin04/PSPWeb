310bd6b24e59d8ec4219e8c6f25f765809ccb37b
frostie <mattwiner04@gmail.com>
2024-10-25 12:31:23 -0700

    Main Lib Changed to Flask

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import *
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html"
+                           
 if __name__ == "__main__":
     app.run(debug=True)
